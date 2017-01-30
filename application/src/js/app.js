763ad921eb578a4e3c9d8ec12fd025fafd546f84
Christian Irack <christian@irack.mx>
2017-01-29 22:40:51 -0600

    Avance con parallax y scss

--- application/src/js/app.js
+++ application/src/js/app.js
@@ -2,22 +2,27 @@ $(function() {
 
 
 
-    ///////////////////////////////////SVG 
+
+    
+    /*----------  SVG  ----------*/
     if ($("#svg1").attr('id')) {
         myVivus = new Vivus('svg1', {
-            duration: 200,
-            reverseStack: false,
-            animTimingFunction: Vivus.EASE
+            duration: 50
 
         }, vivusCall);
-        myVivus.play(1);
-
+        myVivus.stop();
         function vivusCall() {
             //myVivus = new Vivus('my-svg-id',{duration: 200});
             // myVivus.play(1);
         }
     }
-    ////////////////////////////////////BANNER SLIDER HOME
+
+
+
+
+
+
+    /*----------  Slider home con Swiper  ----------*/
     var swiper = new Swiper('.swiper-container', {
         pagination: '.swiper-pagination',
         paginationClickable: true,
@@ -26,8 +31,158 @@ $(function() {
         parallax: true,
         autoplay: 2000,
         speed: 600
-    });   
+    });
+
+
+
+
+
+
+
+
+
+    /*----------  ScrollMagic FX  ----------*/
+    var controller = new ScrollMagic.Controller();
+    /*----------  Menu pegajoso ----------*/     
+
+    var scene = new ScrollMagic.Scene( )
+                .setPin("#menu-sticky")
+                .addIndicators({name: "Menú"}) // add indicators (requires plugin)
+                .addTo(controller);
+   /*----------  Sombra del menu  ----------*/
+    var scene2 = new ScrollMagic.Scene({offset: 100})
+                .addIndicators({name: "debugger"}) // add indicators (requires plugin)
+                .addTo(controller);
+
+    scene2.on("enter", function (event) {
+        //console.log("Scene entered.");
+        $("#sombra-emulada").css('opacity',1);
+    });
+    scene2.on("leave", function (event) {
+        //console.log("Scene left." +event.state);
+       $("#sombra-emulada").css('opacity',0);
+    });
+
+
+
+
+
+
+   /*----------  Animación SVG ----------*/
+
+    var scene3 = new ScrollMagic.Scene({triggerHook:0.7, triggerElement: "#trigger1", duration: 350})
+                .addIndicators({name: "svg"}) // add indicators (requires plugin)
+                .addTo(controller);
+
+        scene3.on("leave", function (event) {
+        //console.log("Scene: " +event.state);
+        //console.log("scrollDirection: " +event.scrollDirection);
+         if(event.state=='BEFORE'){
+            myVivus.play(-1);
+        }
+        
+        //$("#sombra-emulada").css('opacity',0);
+        });
+
+        scene3.on("enter", function (event) {
+        //console.log("Scene entered.");
+            myVivus.play();
+        });
+
+    /*----------  Stiky menu  ----------*/
+    new ScrollMagic.Scene({triggerElement: "#trigger_sec1", duration: 450})
+                    .setClassToggle("#sec1", "active") // add class toggle
+                    .addIndicators({name: "menu-sec1"}) // add indicators (requires plugin)
+                    .addTo(controller);
+    new ScrollMagic.Scene({triggerElement: "#trigger_sec2", duration: 350})
+                    .setClassToggle("#sec2", "active") // add class toggle
+                    .addIndicators({name: "menu-sec2"}) // add indicators (requires plugin)
+                    .addTo(controller);
     
-    ///////////////////////////////////FOUNDATIONO
+
+
+
+
+
+
+
+
+
+
+
+
+    /*----------  Recargar si se escala  ----------*/
+    $(window).resize(function(event) {
+      location.reload();
+    });
+
+
+
+
+
+
+    /*----------  Capas de animacion animadas  ----------*/
+    inicializaAnimaciones()
+     animacionesHome();
+
+
+
+
+
+
+
+      /*----------  Parallax home ----------*/
+
+
+    var controller2 = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
+    new ScrollMagic.Scene({triggerElement: "#parallax1"})
+                    .setTween("#parallax1 > div", {y: "100%", ease: Linear.easeNone})
+                    .addIndicators({name: "home parallax 1"})
+                    .addTo(controller2);
+
+    var controller3 = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "300%"}});
+    new ScrollMagic.Scene({triggerElement: "#parallax2"})
+                    .setTween("#parallax2 > div", {y: "50%",ease: Linear.easeNone})
+                    .addIndicators({name: "home parallax 2"})
+                    .addTo(controller3);
+
+
+
+    var controller4 = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
+    new ScrollMagic.Scene({triggerElement: "#parallax3"})
+                    .setTween("#parallax3 > div", {y: "100%", ease: Linear.easeNone})
+                    .addIndicators({name: "home parallax 3"})
+                    .addTo(controller3);
+
+
+
+
+
+    /*----------  Foundation  ----------*/
     $(document).foundation();
 })
+
+
+
+
+
+/*----------  Funciones globales  ----------*/
+
+function updateBox (e) {
+    if (e.type == "enter") {
+       alert(1);
+    } 
+}
+function inicializaAnimaciones(){
+     $('[target]').prepend("<div class='mascara-cont'><div class='mascara1 mascara'></div><div class='mascara2 mascara'></div></div>");
+}
+function animacionesHome(){
+    var num = 0;
+    $('.mascara-cont:visible').each(function(){
+          TweenLite.fromTo($(this), 1.5, { x: '0%' }, { x: '101%', delay:num, ease: Expo.easeInOut});
+           num += .08;
+    })
+   
+}
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,8 @@ concat = require('gulp-concat'),
 minify = require('gulp-minify'),
 concatCss = require('gulp-concat-css'),
 cssmin = require('gulp-cssmin'),
+cssToJs = require('gulp-css-to-js'),
 run = require('gulp-run');
-////////////////////////////////////
 /*
 Estructura 
 ----------------------------
@@ -19,18 +19,29 @@ Estructura
 ./application/src/scss
 
 */
-////////////////////////////////////
 const scss = './application/src/scss/';
 
 const sjss = './application/src/js/';
 
 const public = './public/';
 // en public
-var cssFiles = ['swiper.css','foundation.css','app.css'];
+var cssFiles = ['swiper.css',
+                'gFonts.css',
+                'foundation.css',
+                'app.css'];
 // en application/src
-var jsFiles = ['vendor/jquery.js','vendor/what-input.js','vendor/foundation.js','swiper.js','vivus.min.js','app.js'];
+var jsFiles = ['vendor/jquery.js',
+                'vendor/what-input.js',
+                'vendor/foundation.js',
+                'swiper.js',
+                'vivus.min.js',
+                'TweenMax.js',
+                'ScrollMagic.js',
+                'plugins/debug.addIndicators.js',
+                'plugins/animation.gsap.js',
+                'app.js'];
 
-////////////////////////////////////
+/*----------  Incluir scripts  ----------*/
 var css = [];
 for(var i in cssFiles){
   css.push(public+'css/'+cssFiles[i]);
@@ -39,7 +50,7 @@ var js = [];
 for(var i in jsFiles){
   js.push(sjss+jsFiles[i]);
 }
-///////////////////////////////
+/*----------  Errores al compilar  ----------*/
 function handleError(err){
    // var cmd1 = new run.Command('clear');
    // cmd1.exec();
@@ -48,31 +59,43 @@ function handleError(err){
     console.log(err.toString());
     this.emit('end');
 }
-/////////////////////////////
+/*----------  Compilar SASS  ----------*/
+
 gulp.task('sass', ()=>
-	gulp.src(scss+'/**/*.scss').on('error', handleError)
+	gulp.src(scss+'/app.scss').on('error', handleError)
 	.pipe(sass({ outputStyle:'expended'})).on('error', handleError)
 	.pipe(autoprefixer({versions:['last 5 browsers']})).on('error', handleError)
 	.pipe(gulp.dest(public+'css/'))
     
 );
-///////////////////////////
+/*----------  Concatenar CSS  ----------*/
 gulp.task('concatcss',['sass'], ()=>
     gulp.src(css).on('error', handleError)
     .pipe(concatCss('bundle.css')).on('error', handleError)
    //.pipe(cssmin()).on('error', handleError)
     .pipe(gulp.dest(public+'css/'))
+    /////////
+
+
     
 );
-///////////////////////////
+/*----------  Concatenar JS  ----------*/
 gulp.task('js', ()=>{
-  return gulp.src(js)
+    gulp.src(js)
     .pipe(concat('bundle.js')).on('error', handleError)
     .pipe(gulp.dest(public+'js/'))
+    /////////
 });
-/////////////////
+
+/*----------  Concatenar JS  ----------*/
+
+
+
+
+/*----------  Vigilar JS, CSS Y SASS  ----------*/
 gulp.task('watch', () => {
 	gulp.watch([public+'css/**/*.css',scss+'**/*.scss'],['concatcss']);
   gulp.watch(sjss+'**/*.js',['js']);
+  //gulp.watch([public+'css/bundle.css',public+'js/bundle.js'],['bundleAll']);
 });
 gulp.task('default', ['concatcss','js','watch']);
