a8dfd0a8898c7056b1a4a1f35518eaa8e25e0ddb
Christian Irack <christian@irack.mx>
2017-02-08 02:54:00 -0600

    cambio de typos

--- application/controllers/Barniz.php
+++ application/controllers/Barniz.php
@@ -3,7 +3,7 @@ class Barniz extends CI_Controller{
 	public function index($info = ''){
 		include_once(APPPATH.'controllers/Twig.php'); 
 		$template = $twig->loadTemplate('home.twig');
-	    echo $template->render(['APPPATH'=>FCPATH,'sec' => 'home']);
+	    echo $template->render(['sec' => 'home']);
 	}
 	public function test(){
 		print('test');
